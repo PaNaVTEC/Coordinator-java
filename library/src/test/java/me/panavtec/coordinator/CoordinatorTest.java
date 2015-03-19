5e6b5fe37c703e22baeb483053c005be78739923
Christian Panadero Martinez <panavtec@gmail.com>
2015-03-19 19:03:44 +0100

    Testing edge cases

--- library/src/test/java/me/panavtec/coordinator/CoordinatorTest.java
+++ library/src/test/java/me/panavtec/coordinator/CoordinatorTest.java
@@ -1,27 +1,33 @@
 package me.panavtec.coordinator;
 
 import me.panavtec.coordinator.listeners.CoordinatorComplete;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
-public class CoordinatorTest implements CoordinatorComplete {
-  
+public class CoordinatorTest {
+
   private static final int AN_ACTION = 1;
   private static final int ANOTHER_ACTION = 2;
+  
   private Coordinator coordinator;
-  
+  @Mock private CoordinatorComplete coordinatorComplete;
+
   @Before public void setUp() {
-    this.coordinator = new Coordinator(this, AN_ACTION, ANOTHER_ACTION);
-  }
-  
-  @Test public void testCoordinatorWithTwoActions() {
-    coordinator.completeAction(AN_ACTION);
-    coordinator.completeAction(ANOTHER_ACTION);
-    Assert.fail("Completed bad");
+    MockitoAnnotations.initMocks(this);
+    this.coordinator = new Coordinator(coordinatorComplete, AN_ACTION, ANOTHER_ACTION);
   }
 
-  @Override public void onCoordinatorComplete() {
-    
+  @Test public void completeCalled() {
+    coordinator.completeAction(AN_ACTION);
+    coordinator.completeAction(ANOTHER_ACTION);
+    Mockito.verify(coordinatorComplete).onCoordinatorComplete();
+  }
+
+  @Test public void notCalledComplete() {
+    coordinator.completeAction(AN_ACTION);
+    Mockito.verifyNoMoreInteractions(coordinatorComplete);
   }
 }
